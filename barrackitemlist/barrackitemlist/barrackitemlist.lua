22a56c0bc86850ab9283531e9b5fe3b53435f4fc
Rafael BSIDES Pereira <rafaelbsides@gmail.com>
2017-04-30 08:09:05 -0300

    Search ignore case
    I was very annoyed by searching only in exact names. Next step will be
    some fuzzy search.

--- barrackitemlist/barrackitemlist/barrackitemlist.lua
+++ barrackitemlist/barrackitemlist/barrackitemlist.lua
@@ -232,7 +232,7 @@ function BARRACKITEMLIST_SEARCH_ITEMS(itemlist,itemName,iswarehouse)
             for group,list in pairs(itemlist[cid]) do
                 if group ~= 'warehouse' or iswarehouse then
                     for i ,v in ipairs(list) do
-                        if string.find(v[1],itemName) then
+                        if string.find(string.lower(v[1]),string.lower(itemName)) then
                             items[cid] = items[cid] or {}
                             table.insert(items[cid],v)
                         end
